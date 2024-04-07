596c5b72c05440468bae8504060b89509e8712f6
Scoob1 <tamara_walling@yahoo.com>
2024-04-07 03:58:05 +0000

    added 0-main

---
+++ main_0.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void bubble_sort(int *array, size_t size);
+void print_array(int *array, size_t size);
+
+int main()
+{
+	int array[] = {5, 3, 8, 1, 6, 2, 7, 4};
+	size_t size = sizeof(array) / sizeof(array[0]);
+
+	printf("Original array:\n");
+	print_array(array, size);
+
+	bubble_sort(array, size);
+
+	printf("Sorted array:\n");
+	print_array(array, size);
+
+	return (0);
+}
