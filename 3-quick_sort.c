bbcad77a7390a85e2a9c123c01b3359ee1c121f6
Scoob1 <tamara_walling@yahoo.com>
2024-04-08 16:05:29 +0000

    task 3 addition

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -41,12 +41,44 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 			}
 		}
 	}
-
 	if (array[i + 1] != array[high])
 	{
-	    swap(&array[i + 1], &array[high]);
-	    print_array(array, size);
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
 	}
-
 	return (i + 1);
+}
+
+/**
+ * quick_sort_recursive - Recursive function to implement Quick sort
+ * @array: Array to sort
+ * @low: Starting index of the partition
+ * @high: Ending index of the partition
+ * @size: Size of the array
+ */
+
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pivot_index = lomuto_partition(array, low, high, size);
+
+		quick_sort_recursive(array, low, pivot_index - 1, size);
+		quick_sort_recursive(array, pivot_index + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using
+ *              the Quick sort algorithm with Lomuto partition scheme
+ * @array: Array to sort
+ * @size: Size of the array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quick_sort_recursive(array, 0, size - 1, size);
 }
--- sort.h
+++ sort.h
@@ -26,5 +26,8 @@ void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void swap(int *a, int *b);
+int lomuto_partition(int *array, int low, int high, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
