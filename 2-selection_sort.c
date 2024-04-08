5ccc73976a064f2b05ffb267f0d29935d554e066
Scoob1 <tamara_walling@yahoo.com>
2024-04-08 14:23:15 +0000

    moved things around placementt wise

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,13 +10,11 @@
 
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j, min_idx;
+	int temp;
+
 	if (array == NULL || size < 2)
 		return;
-
-	size_t i;
-	size_t j;
-	size_t min_idx;
-	int temp;
 
 	for (i = 0; i < size - 1; i++)
 	{
