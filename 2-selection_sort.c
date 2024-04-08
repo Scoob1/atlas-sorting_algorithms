0db711b73364df05300479cb07e4413248913d03
Scoob1 <tamara_walling@yahoo.com>
2024-04-08 14:01:57 +0000

    = instead of !=

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -21,7 +21,7 @@ void selection_sort(int *array, size_t size)
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
-				min_idx != j;
+				min_idx = j;
 		}
 
 		if (min_idx != i)
