4bde5c1ad1a8c669e091eb3423fd152711ef61e8
Scoob1 <tamara_walling@yahoo.com>
2024-04-08 04:57:07 +0000

    insertion instead of inertion in header

--- sort.h
+++ sort.h
@@ -24,6 +24,6 @@ listint_t;
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
-void inertion_sort_list(listint_t **list);
+void insertion_sort_list(listint_t **list);
 
 #endif
